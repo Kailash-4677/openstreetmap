6a6a7077c73e84f2517bc375d047e45c51f9b9eb
Kailash-4677 <kailashsrinivasan46@gmail.com>
2025-02-18 11:49:02 +0530

    Adding Current location route to get the last updated location

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
 # Initialize GPS data with default location (Chennai)
-gps_data = {"latitude": 13.356872, "longitude": 80.142214}
+gps_data = {"latitude": 13.323407, "longitude": 80.152771}
 
 @app.route('/')
 def index():
@@ -20,6 +20,12 @@ def update_location():
         print(f"📍 GPS Updated: Latitude: {lat}, Longitude: {lon}")
 
     return "Location updated successfully"
+
+@app.route('/current_location', methods=['GET'])
+def current_location():
+    print("currentlocation")
+    print(jsonify(gps_data))
+    return jsonify(gps_data)  # Returns the latest coordinates as JSON
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
